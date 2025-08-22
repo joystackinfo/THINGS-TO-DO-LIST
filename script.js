e01cbc49b3fb87b12af8cb55d2681148a6c492b1
joystackinfo <joynelsoninfo@gmail.com>
2025-08-22 15:09:34 +0100

    added another

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const form = document.getElementById("task-form");  // to get the form element
 const taskinput = document.getElementById("task-input"); //to get the taskinput element
 const tasklist = document.getElementById("task-list"); // to get the tasklist element
 
+
+
+
