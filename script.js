08885a73cac199b5e27fc0c026da16780d7ecbaa
joystackinfo <joynelsoninfo@gmail.com>
2025-08-22 15:13:42 +0100

    added a if statement

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.getElementById("task-form");  // to get the form element
 const taskinput = document.getElementById("task-input"); //to get the taskinput element
 const tasklist = document.getElementById("task-list"); // to get the tasklist element
 
-
-
-
+function addTask(){
+    if (taskinput.value === "" ) {
+        alert("Please enter a task.");
+        return;
+   
